d08c752977c3ff5ced46528d77bd9a6c2848d6f4
emilssolmanis <emils@last.fm>
2014-12-13 16:31:19 +0000

    Hacked together google fitness API call

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var mb = require('musicbrainz');
 var request = require('request');
 var config = require('./config');
 var SpotifyWebApi = require('spotify-web-api-node');
+var google = require('googleapis');
+var fitness = google.fitness('v1');
+
+var OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2(config.google.clientId, config.google.clientSecret, config.google.oauthRedirectUrl);
 
 var lastfm = new LastFmNode({
     api_key: config.api_key,
@@ -14,29 +20,95 @@ var spotify = new SpotifyWebApi({
     clientSecret: config.spotify.secret
 });
 
-lastfm.request('user.topTracks', {
-    limit: 10,
-    user: 'EsmuPliks',
-    handlers: {
-        success: handleTopTracks,
-        error: function(error) {
-            console.error('Failed fetching from Last.fm API', error);
+if (!config.google.tempAuthCode) {
+    var scopes = [
+        'https://www.googleapis.com/auth/fitness.activity.read',
+        'https://www.googleapis.com/auth/fitness.activity.write',
+        'https://www.googleapis.com/auth/fitness.body.read',
+        'https://www.googleapis.com/auth/fitness.body.write',
+        'https://www.googleapis.com/auth/fitness.location.read',
+        'https://www.googleapis.com/auth/fitness.location.write'
+    ];
+
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+        scope: scopes // If you only need one scope you can pass it as string
+    });
+
+    console.log('oauth auth url %s', url);
+    console.log('make sure you have a server running on ', config.google.oauthRedirectUrl);
+} else if (!config.google.tokens.auth) {
+    oauth2Client.getToken(config.google.tempAuthCode, function (err, tokens) {
+        // Now tokens contains an access_token and an optional refresh_token. Save them.
+        if (!err) {
+            console.log(tokens);
+            oauth2Client.setCredentials(tokens);
+            google.options({auth: oauth2Client});
+
+            fetchGoogleFitness(function (err, response) {
+                if (!err) {
+                    console.log(response.body);
+                } else {
+                    console.error('Error listing users datasources', err);
+                }
+            });
+            //fetchLastfmTopTracks();
+        } else {
+            console.error('Failed to auth against Google stuff', err);
         }
-    }
-});
+    });
+} else {
+    oauth2Client.setCredentials({
+        access_token: config.google.tokens.auth,
+        token_type: 'Bearer',
+        expiry_date: config.google.tokens.expiry,
+        refresh_token: config.google.tokens.refresh
+    });
+    google.options({auth: oauth2Client});
+
+    fetchGoogleFitness(function (err, response) {
+        if (!err) {
+            console.log(response);
+        } else {
+            console.error('Error listing users datasources', err);
+        }
+    });
+    //fetchLastfmTopTracks();
+}
+
+function fetchGoogleFitness(callback) {
+    fitness.users.dataSources.datasets.get({
+        userId: 'me',
+        dataSourceId: 'derived:fm.last.heart_rate:407408718192:Example Manufacturer:ExampleTablet:1000001:music hack day',
+        datasetId: '1411053997000000000-1411057556000000000'
+    }, callback);
+}
+
+function fetchLastfmTopTracks() {
+    lastfm.request('user.topTracks', {
+        limit: 10,
+        user: 'EsmuPliks',
+        handlers: {
+            success: handleTopTracks,
+            error: function (error) {
+                console.error('Failed fetching from Last.fm API', error);
+            }
+        }
+    });
+}
 
 function handleTopTracks(response) {
     var tracksWithMbid = response.toptracks.track.filter(function (e) {
         return e.mbid;
     });
 
-    sortByBpm(tracksWithMbid, function(tracks) {
+    sortByBpm(tracksWithMbid, function (tracks) {
         createPlaylist(tracks, console.log);
     });
 }
 
 function createPlaylist(tracks, callback) {
-    appendSpotifyUris(tracks, function() {
+    appendSpotifyUris(tracks, function () {
         var playlist = 'spotify:trackset:Playlist:' + tracks.map(function (t) {
                 return t.spotifyUri;
             }).join(',');
@@ -46,22 +118,23 @@ function createPlaylist(tracks, callback) {
 
 function appendSpotifyUris(tracks, callback) {
     var trackCache = [];
+
     function addResolved(track) {
         trackCache.push(track);
         if (trackCache.length === tracks.length) {
-            callback(trackCache.filter(function(track) {
+            callback(trackCache.filter(function (track) {
                 return track.spotifyUri
             }));
         }
     }
 
-    tracks.forEach(function(track) {
+    tracks.forEach(function (track) {
         spotify.searchTracks('artist:' + track.artist.name + ' track:' + track.name)
-            .then(function(data) {
+            .then(function (data) {
                 var item = data.tracks.items[0]
                 track.spotifyUri = item.id;
                 addResolved(track);
-            }, function(err) {
+            }, function (err) {
                 console.log('Failed getting spotify uri for ' + track.name);
                 addResolved(track);
             });
@@ -70,20 +143,21 @@ function appendSpotifyUris(tracks, callback) {
 
 function sortByBpm(tracks, callback) {
     var trackCache = [];
+
     function addResolved(track) {
         trackCache.push(track);
         if (trackCache.length === tracks.length) {
             callback(trackCache.filter(function (t) {
-                return t.bpm;
-            }).sort(function (a, b) {
-                return a.bpm - b.bpm;
-            })
-        );
+                    return t.bpm;
+                }).sort(function (a, b) {
+                    return a.bpm - b.bpm;
+                })
+            );
         }
     }
 
     tracks.forEach(function (track) {
-        mb.lookupRecording(track.mbid, [], function(err, release) {
+        mb.lookupRecording(track.mbid, [], function (err, release) {
             if (err) {
                 addResolved(track);
             }
