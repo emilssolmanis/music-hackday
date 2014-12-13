e3daa2a36c8fa714d442147121de3ddd2a2fc686
emilssolmanis <emils@last.fm>
2014-12-13 12:19:47 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,40 @@
+var LastFmNode = require('lastfm').LastFmNode;
+var mb = require('musicbrainz');
+var request = require('request');
+var config = require('config');
+
+var lastfm = new LastFmNode({
+    api_key: config.api_key,
+    secret: config.secret
+});
+
+lastfm.request('user.topTracks', {
+    limit: 20,
+    user: 'EsmuPliks',
+    handlers: {
+        success: handleTopTracks,
+        error: function(error) {
+            console.error('Failed fetching from Last.fm API', error);
+        }
+    }
+});
+
+function handleTopTracks(response) {
+    response.toptracks.track.filter(function(e) { return e.mbid; }).forEach(function (track) {
+        mb.lookupRecording(track.mbid, [], function(err, release) {
+            if (err) {
+                return;
+            }
+            request({
+                url: 'http://acousticbrainz.org/' + release.id + '/low-level',
+                json: true
+            }, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log('%s - %s: %s', track.artist.name, track.name, body.rhythm.bpm);
+                } else {
+                    console.warn('Failed getting AcousticBrainz %s', track.name);
+                }
+            });
+        })
+    });
+}
